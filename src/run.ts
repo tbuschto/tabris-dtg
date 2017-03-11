f95f4763abed4d0df635c6789e7c07ebaf4e5c14
Tim Buschtoens <tbuschto@eclipsesource.com>
2017-03-11 17:26:27 +0100

    Parse given xml
    Also add an example xml

--- src/run.ts
+++ src/run.ts
@@ -1,7 +1,26 @@
-import {readFile} from 'fs';
+import {createReadStream} from 'fs';
+import {parser, SAXParser,  Tag, QualifiedTag} from 'sax';
 
-let filename: string = process.argv[2];
+const filename = process.argv[2];
 
-readFile(filename, 'utf8', (err, data) => {
-  console.log(data)
-});
+if (!filename) {
+  console.log('No file given');
+}
+
+const readStream = createReadStream(filename, {encoding: 'utf-8'});
+const saxParser = createSaxParser();
+
+readStream.addListener('data', (chunk) => saxParser.write(chunk));
+readStream.addListener('end', () => saxParser.close())
+readStream.addListener('error', (error) => console.error(error));
+
+function createSaxParser(): SAXParser {
+  const result = parser(true, {});
+  result.onopentag = (tag: Tag  | QualifiedTag) => {
+    console.log(`<${tag.name}>`);
+  };
+  result.onclosetag = (tag: string) => {
+    console.log(`</${tag}>`);
+  };
+  return result;
+}
