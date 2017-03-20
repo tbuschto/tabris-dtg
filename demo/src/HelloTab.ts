bbee0776917e56498214654eb31c854b9ca33bb7
Tim Buschtoens <tbuschto@eclipsesource.com>
2017-03-20 23:30:29 +0100

    Add some interactivity to HelloTab

--- demo/src/HelloTab.ts
+++ demo/src/HelloTab.ts
@@ -4,6 +4,9 @@ export default class extends HelloTabUI {
 
   constructor(properties?) {
     super(properties);
+    this.button.on('select', () => {
+      this.message.text += ' World!';
+    });
   }
 
 }
